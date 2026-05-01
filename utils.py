edc42616e5a24e85b6f5f10ab6b255557f2b594a
Cyrus Orbell <orbell.cyrus@gmail.com>
2026-05-01 14:39:29 -0500

    f

--- utils.py
+++ utils.py
@@ -23,36 +23,7 @@ def accuracy(y_true, y_pred):
     return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
 
 def get_data(dir):
-    X_train, y_train = [], []
-    X_test, y_test = [], []
-
-    # loop through s1 → s40
-    for i in range(1, 41):
-        folder = os.path.join(dir, f"s{i}")
-        images = sorted([f for f in os.listdir(folder) if f.endswith(".pgm")])
-
-        for img_name in images:
-            path = os.path.join(folder, img_name)
-
-            # load image
-            img = load_img(path, color_mode="grayscale")
-            img_array = img_to_array(img).squeeze()  # (H, W)
-
-            # assign to train or test
-            if i <= 35:
-                X_train.append(img_array)
-                y_train.append(i)
-            else:
-                X_test.append(img_array)
-                y_test.append(i)
-
-    # convert to numpy arrays (important for ML)
-    X_train = np.array(X_train)
-    y_train = np.array(y_train)
-    X_test = np.array(X_test)
-    y_test = np.array(y_test)
-
-    return X_train, y_train, X_test, y_test
+    # ** YOUR CODE HERE **
 
     
 
